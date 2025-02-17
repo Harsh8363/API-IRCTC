0a33c27bfa5c78eecef399a42d4b12fd88ae2709
Harshit Verma <harshverma.vvip@gmail.com>
2025-02-18 02:14:14 +0530

    Added Cancel ticket functionality

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -57,6 +57,49 @@ exports.bookSeat = async (req, res, next) => {
 };
 
 
+exports.cancelSeat = async (req, res, next) => {
+  const connection = await db.getConnection();
+  await connection.beginTransaction();
+
+  try {
+    const { trainId,bookingId, seatCount } = req.body;
+    const userId = req.user.userId;
+
+    // Check if train exists and lock it for update
+    const [trainRows] = await connection.query(
+      "SELECT * FROM trains WHERE id = ? FOR UPDATE",
+      [trainId]
+    );
+    const [bookingRows] = await connection.query(
+      "SELECT * FROM bookings WHERE id = ? FOR UPDATE",
+      [bookingId]
+    );
+    const train = trainRows[0];
+    const booking = bookingRows[0];
+
+    // Update available seats
+    const newAvailableSeats = train.available_seats + seatCount;
+    await connection.query(
+      "UPDATE trains SET available_seats = ? WHERE id = ?",
+      [newAvailableSeats, trainId]
+    );
+    
+    await connection.query(
+      "DELETE FROM bookings  WHERE id = ?",
+      [bookingId]
+    );
+
+    await connection.commit();
+    res.status(201).json("Booking Cancelled");
+  } catch (err) {
+    await connection.rollback();
+    next(err);
+  } finally {
+    connection.release();
+  }
+};
+
+
 
 exports.getBookingDetails = async (req, res, next) => {
   try {
@@ -64,7 +107,7 @@ exports.getBookingDetails = async (req, res, next) => {
     const { trainId } = req.query;
 
     // Find booking by user ID and train ID
-    const booking = await Booking.findByUserIdAndTrainId(userId, trainId);
+    const booking = await Booking.findByUserIdAndTrainId(userId, trainId,db);
     if (!booking) {
       return res.status(404).json({ error: "Booking not found" });
     }
@@ -74,3 +117,5 @@ exports.getBookingDetails = async (req, res, next) => {
     next(err);
   }
 };
+
+ 
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,6 +4,7 @@ const bookingController = require("../controllers/bookingController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 router.post("/book", authMiddleware.verifyToken, bookingController.bookSeat);
+router.post("/cancel",authMiddleware.verifyToken,bookingController.cancelSeat);
 router.get(
   "/details",
   authMiddleware.verifyToken,
